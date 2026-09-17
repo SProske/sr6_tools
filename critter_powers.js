d57decf84431fafe605c2915b7f5d5da763703e7
SProske <proske.sebastian@gmail.com>
2026-09-17 10:54:24 +0200

    Update and rename powers.js to critter_powers.js

--- critter_powers.js
+++ critter_powers.js
@@ -1,22 +1,4 @@
-export const ELEMENT_STATUS_MAP = {
-    "Feuer": "Brennend",
-    "Elektrizität": "Gebrutzelt",
-    "Chemisch": "Verätzt",
-    "Kälte": "Unterkühlt",
-    "Strahlung": "Verstrahlt"
-};
-
-export const statusData = {
-    "Panisch": "Lorem ipsum dolor sit amet: Das Ziel muss seine Handlungen nutzen, um sich aus der Gefahrenzone zu bewegen.",
-    "Verängstigt": "Lorem ipsum dolor sit amet: Das Ziel erhält einen Abzug von -2 auf alle Proben.",
-    "Verwirrt": "Lorem ipsum dolor sit amet: Das Ziel erleidet Würfelabzüge in Höhe der Stufe des Status.",
-    "Benommen": "Lorem ipsum dolor sit amet: Das Ziel verliert seine nächste Haupthandlung.",
-    "Gebrutzelt": "Lorem ipsum: Statuseffekt durch Elektrizitätsschaden.",
-    "Brennend": "Lorem ipsum: Statuseffekt durch Feuerschaden.",
-    "Verätzt": "Lorem ipsum: Statuseffekt durch Säure-/Chemieschaden.",
-    "Unterkühlt": "Lorem ipsum: Statuseffekt durch Kälteschaden.",
-    "Verstrahlt": "Lorem ipsum: Statuseffekt durch Strahlungsschaden."
-};
+import { ELEMENT_STATUS_MAP } from './status.js';
 
 export const powerData = {
     "Grauen": {
